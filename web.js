5b1b53be17356504eb7df1acdc33dc772d749c7f
Hein Saris <hein@saris.be>
2013-07-18 13:11:38 +0000

    homework 4 - part 1

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express'),
     fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
